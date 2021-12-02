673f0c2f29384c8bf7a144ff9c248a081763da35
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-12-02 13:09:14 +0000

    updates and bug fixes

--- src/interfaces/artist.ts
+++ src/interfaces/artist.ts
@@ -1,14 +1,14 @@
 interface Artist {
     id: number;
-            name: string;
-            "link": "https://www.deezer.com/artist/3933641",
-            "picture": "https://api.deezer.com/artist/3933641/image",
-            "picture_small": "https://e-cdns-images.dzcdn.net/images/artist/0a5d34bab78b51067698dd51767154aa/56x56-000000-80-0-0.jpg",
-            "picture_medium": "https://e-cdns-images.dzcdn.net/images/artist/0a5d34bab78b51067698dd51767154aa/250x250-000000-80-0-0.jpg",
-            "picture_big": "https://e-cdns-images.dzcdn.net/images/artist/0a5d34bab78b51067698dd51767154aa/500x500-000000-80-0-0.jpg",
-            "picture_xl": "https://e-cdns-images.dzcdn.net/images/artist/0a5d34bab78b51067698dd51767154aa/1000x1000-000000-80-0-0.jpg",
-            "tracklist": "https://striveschool-api.herokuapp.com/api/deezer/artist/3933641/top?limit=50",
-            "type": "artist"
+            name: string
+            link: string
+            picture: string
+            picture_small: string
+            picture_medium: string
+            picture_big: string
+            picture_xl: string
+            tracklist: string
+            type: string
 }
 
 export default Artist
